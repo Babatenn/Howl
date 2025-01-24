25b9d005b7026e2eacf050567e8eb62c15fa265f
Babatenn <131091935+Babatenn@users.noreply.github.com>
2025-01-24 22:54:53 +0900

    Update Q1.js

--- Q1.js
+++ Q1.js
@@ -1,3 +1,3 @@
 function nazo()
-{if (document.getElementById('answer').value == 'となりのトトロ'){alert('あたり!');}
-　else{alert('はずれ');}}
+{if (document.getElementById('answer').value == 'となりのトトロ'){alert('正解!');}
+　else{alert('残念。不正解！');}}
