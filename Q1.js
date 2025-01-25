71d7cf18a44d99758c72b44471d1fe5ab0f855a2
Babatenn <131091935+Babatenn@users.noreply.github.com>
2025-01-25 21:56:23 +0900

    Update Q1.js

--- Q1.js
+++ Q1.js
@@ -1,3 +1,6 @@
 function nazo()
 {if (document.getElementById('answer').value == 'となりのトトロ'){alert('正解!');}
 　else{alert('残念。不正解！');}}
+
+function imgchange1() 
+{document.getElementById('answer').src = './image/トトロ.jpg';}
