558353ac52f55a9a8ef947488e53ee0bfb27d853
Babatenn <131091935+Babatenn@users.noreply.github.com>
2025-01-25 21:58:10 +0900

    Update Q4.js

--- Q4.js
+++ Q4.js
@@ -1,3 +1,6 @@
 function nazo4()
 {if (document.getElementById('answer4').value == 'ハウルの動く城'){alert('正解!');}
 　else{alert('残念。不正解！');}}
+
+function imgchange4() 
+{document.getElementById('answer4').src = './image/ハウル.jpg';}
