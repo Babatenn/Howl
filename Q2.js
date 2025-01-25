989b1815b3f73010c946e546bfe3cd13bf5f68e3
Babatenn <131091935+Babatenn@users.noreply.github.com>
2025-01-25 21:56:57 +0900

    Update Q2.js

--- Q2.js
+++ Q2.js
@@ -1,3 +1,6 @@
 function nazo2()
 {if (document.getElementById('answer2').value == '魔女の宅急便'){alert('正解!');}
 　else{alert('残念。不正解！');}}
+
+function imgchange2() 
+{document.getElementById('answer2').src = './image/魔女.jpg';}
