fa6ef67044a922c60262de2336d85b592b3ee9e1
Babatenn <131091935+Babatenn@users.noreply.github.com>
2025-01-25 21:57:27 +0900

    Update Q3.js

--- Q3.js
+++ Q3.js
@@ -1,3 +1,6 @@
 function nazo3()
 {if (document.getElementById('answer3').value == '千と千尋の神隠し'){alert('正解!');}
 　else{alert('残念。不正解！');}}
+
+function imgchange3() 
+{document.getElementById('answer3').src = './image/千.jpg';}
