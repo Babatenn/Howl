5bf91563362ea6f57cd3ed4879200e97b53aa199
Babatenn <131091935+Babatenn@users.noreply.github.com>
2025-01-25 21:58:59 +0900

    Update Q5.js

--- Q5.js
+++ Q5.js
@@ -1,3 +1,6 @@
 function nazo5()
 {if (document.getElementById('answer5').value == '借りぐらしのアリエッティ'){alert('正解!');}
 　else{alert('残念。不正解！');}}
+
+function imgchange5() 
+{document.getElementById('answer5').src = './image/アリエッティ.jpg';}
