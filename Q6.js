a8a80ac66d7162540b7a3d5500d0d123282a7bef
Babatenn <131091935+Babatenn@users.noreply.github.com>
2025-01-25 22:00:01 +0900

    Update Q6.js

--- Q6.js
+++ Q6.js
@@ -1,3 +1,6 @@
 function nazo6()
 {if (document.getElementById('answer6').value == '君たちはどう生きるか'){alert('正解!');}
 　else{alert('残念。不正解！');}}
+
+function imgchange6() 
+{document.getElementById('answer6').src = './image/君たちは.jpg';}
