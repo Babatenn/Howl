447d366add717cc5cf4857137e97c79c26acd3dd
Babatenn <131091935+Babatenn@users.noreply.github.com>
2025-01-25 22:15:40 +0900

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,2 +1,2 @@
-function imgchange() {document.getElementById('ghibli').src = './image/totoro.jpg';}
+function imagechange() {document.getElementById('ghibli').src = './image/totoro.jpg';}
 function mes1() {document.getElementById('ghibli').src = './image/ジブリ１.png';}
